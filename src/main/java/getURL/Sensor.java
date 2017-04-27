42ff179457fc087043cfe1659c35ef611e8b2e20
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2017-04-27 14:24:51 +0200

    builder pattern add to Sensor

--- src/main/java/getURL/Sensor.java
+++ src/main/java/getURL/Sensor.java
@@ -9,57 +9,112 @@ public class Sensor {
 	int value;
 	int min_value;
 	int max_value;
-	
-	/**
-	 * 
-	 * Getters and setters
-	 */
+
+	private Sensor(final Builder builder) {
+		this.id = builder.id;
+		this.engine = builder.engine;
+		this.masterSensorId = builder.masterSensorId;
+		this.type = builder.type;
+		this.name = builder.name;
+		this.value = builder.value;
+		this.min_value = builder.min_value;
+		this.max_value = builder.max_value;
+	}
+
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public int getEngine() {
 		return engine;
 	}
-	public void setEngine(int engine) {
-		this.engine = engine;
-	}
+
 	public String getType() {
 		return type;
 	}
-	public void setType(String type) {
-		this.type = type;
-	}
+
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
+
 	public int getValue() {
 		return value;
 	}
-	public void setValue(int value) {
-		this.value = value;
-	}
+
 	public int getMin_value() {
 		return min_value;
 	}
-	public void setMin_value(int min_value) {
-		this.min_value = min_value;
-	}
+
 	public int getMax_value() {
 		return max_value;
 	}
-	public void setMax_value(int max_value) {
-		this.max_value = max_value;
-	}
+
 	public int getMasterSensorId() {
 		return masterSensorId;
 	}
-	public void setMasterSensorId(int masterSensorId) {
-		this.masterSensorId = masterSensorId;
+
+	public static class Builder {
+		private final int id;
+		private int engine;
+		private int masterSensorId;
+		private String type;
+		private String name;
+		private int value;
+		private int min_value;
+		private int max_value;
+
+		public Builder(final Object object) {
+			this.id = Integer.valueOf((String) object);
+		}
+
+		public Builder engine(final Object object) {
+			if (object != null) {
+				this.engine = Integer.valueOf((String) object);
+				return this;
+			} else {
+				this.engine = 0;
+				return this;
+			}
+		}
+
+		public Builder masterSensorId(final Object object) {
+			if (object != null) {
+				this.masterSensorId = Integer.valueOf((String) object);
+				return this;
+			} else {
+				this.masterSensorId = 0;
+				return this;
+			}
+
+		}
+
+		public Builder type(final Object object) {
+			this.type = (String) object;
+			return this;
+		}
+
+		public Builder name(final Object object) {
+			this.name = (String) object;
+			return this;
+		}
+
+		public Builder value(final Object value) {
+			this.value = Integer.valueOf((String) value);
+			return this;
+		}
+
+		public Builder minValue(final Object min_value) {
+			this.min_value = Integer.valueOf((String) min_value);
+			return this;
+		}
+
+		public Builder maxValue(final Object max_value) {
+			this.max_value = Integer.valueOf((String) max_value);
+			return this;
+		}
+
+		public Sensor build() {
+			return new Sensor(this);
+		}
 	}
 }
