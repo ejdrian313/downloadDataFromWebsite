64139b729bf28bdc2efd74364a4ee4424df61458
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2017-04-29 15:49:45 +0200

    add webReader

--- src/main/java/getURL/MainClass.java
+++ src/main/java/getURL/MainClass.java
@@ -12,44 +12,16 @@ import org.jsoup.nodes.Element;
 public class MainClass {
 
 	public static void main(String[] args) {
+		ArrayList<Sensor> sensors;
 		try {
 			String url;
 			if(args.length > 0) {
 				url = args[0];
 			} else url = "https://github.com/ejdrian313/downloadDataFromWebsite/blob/master/file.yml";
 			
-			Document document = Jsoup.connect(url).get();
-			
-			StringBuilder temp = new StringBuilder();
-			for (Element table : document.select("table[class=highlight tab-size js-file-line-container]")) {
-				for (Element row : table.select("tr")) {
-					if(!row.text().startsWith("-")){
-						temp.append("  ");
-					}
-					temp.append(row.text() + "\n");
-				}
-			}
-
-			YamlReader reader = new YamlReader(new String(temp));
-			
-			ArrayList<?> sensors = (ArrayList<?>) reader.read();
-			sensors.forEach(s -> System.out.println(s));
-			
-			ArrayList<Sensor> sensorList = new ArrayList<>();
-			for(Object o : sensors) {			
-				@SuppressWarnings("rawtypes")
-				Map map = (Map)o;
-				sensorList.add(new Sensor.Builder(map.get("id"))
-						.engine(map.get("engine"))
-						.masterSensorId(map.get("masterSensorId"))
-						.type(map.get("type"))
-						.name(map.get("name"))
-						.value(map.get("value"))
-						.minValue(map.get("min_value"))
-						.maxValue(map.get("max_value"))
-						.build());
-			}
-			sensorList.forEach(s -> System.out.println(s.getValue()));
+			WebReader webReader = new WebReader(url);
+			sensors = new ArrayList<>(webReader.createArrayListFromYaml());
+			sensors.forEach(s -> System.out.println(s.getValue()));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
---
+++ src/main/java/getURL/WebReader.java
@@ -0,0 +1,71 @@
+package getURL;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import com.esotericsoftware.yamlbeans.YamlReader;
+
+public class WebReader {
+	Document document;
+	StringBuilder temp;
+
+	WebReader(String url) {
+		try {
+			document = Jsoup.connect(url).get();
+			readYamlFromGithub();
+		} catch (UnknownHostException u) {
+			System.err.println("Check internet connection");
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	private void readYamlFromGithub() {
+		temp = new StringBuilder();
+		for (Element table : document.select("table[class=highlight tab-size js-file-line-container]")) {
+			for (Element row : table.select("tr")) {
+				if (!row.text().startsWith("-")) {
+					temp.append("  ");
+				}
+				temp.append(row.text() + "\n");
+			}
+		}
+	}
+
+	public ArrayList<Sensor> createArrayListFromYaml() {
+		YamlReader reader = new YamlReader(new String(temp));
+		ArrayList<?> sensors;
+		ArrayList<Sensor> sensorList = new ArrayList<>();
+		try {
+			sensors = (ArrayList<?>) reader.read();
+
+			for (Object o : sensors) {
+				@SuppressWarnings("rawtypes")
+				Map map = (Map) o;
+				sensorList.add(new Sensor.Builder(map.get("id")).
+						engine(map.get("engine"))
+						.masterSensorId(map.get("masterSensorId")).
+						type(map.get("type")).
+						name(map.get("name"))
+						.value(map.get("value")).
+						minValue(map.get("min_value")).
+						maxValue(map.get("max_value")).
+						build());
+			}
+			sensors.clear();
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return sensorList;
+	}
+}
