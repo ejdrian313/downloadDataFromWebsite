fdac517f32a2af543b0a7ac4e7b67588686d8bc5
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2017-04-30 23:53:51 +0200

    sort collections by comparator

--- src/main/java/getURL/MainClass.java
+++ src/main/java/getURL/MainClass.java
@@ -1,17 +1,13 @@
 package getURL;
 
 import java.util.ArrayList;
-import java.util.Map;
-
-import com.esotericsoftware.yamlbeans.YamlReader;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainClass {
 
 	public static void main(String[] args) {
+		long startTime = System.nanoTime();
 		ArrayList<Sensor> sensors;
 		try {
 			String url;
@@ -21,10 +17,22 @@ public class MainClass {
 			
 			WebReader webReader = new WebReader(url);
 			sensors = new ArrayList<>(webReader.createArrayListFromYaml());
-			sensors.forEach(s -> System.out.println(s.getValue()));
+			
+			Collections.sort(sensors, new Comparator<Sensor>() {
+		        public int compare(Sensor s2, Sensor s1)
+		        {
+		            return  Integer.valueOf(s2.getId()).compareTo(s1.getId());
+		        }
+		    });
+			
+			sensors.forEach(s -> System.out.println("Sensor: " + s.getId() +" of " + s.getType() + " current value: " + s.getValue()));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			Long endTime = System.nanoTime();
+			Double duration = (endTime - startTime) / 1000000000.0;
+			System.out.println("I'm out in time: " + String.format("%.2f", duration) + "s");
 		}
 	}
 }
